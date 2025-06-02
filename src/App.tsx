a02a955d9593d514f4ae9609c550fd6ac6b417ae
Cris <kristiano.gon@gmail.com>
2025-06-02 09:47:55 -0300

    chore: setup inventory remote mfe

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
-import InventoryProvider from "./components/InventoryComponent";
 
-const App = () => {
+function App() {
   return (
-    <div className="content">
-      <InventoryProvider />
+    <div className="container">
+      <div className="icon-container">
+        <h2>📊</h2>
+      </div>
+      <h1 className="title">Fiap Farm Inventory</h1>
     </div>
   );
-};
+}
 
 export default App;

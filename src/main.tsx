4cb451f693e01ffbd19a7718d7851dfd597cbf98
IsabellaSoares <isabella.soares.28@gmail.com>
2025-07-06 23:04:44 -0300

    chore: install tanstack

--- src/main.tsx
+++ src/main.tsx
@@ -6,12 +6,16 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./stores/redux/store";
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <QueryClientProvider client={queryClient}>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>
 );

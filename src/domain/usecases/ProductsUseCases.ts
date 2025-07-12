ac3eb3961337745e5a17aaacf972b8c12fbc1ae3
Cris <kristiano.gon@gmail.com>
2025-07-12 16:19:10 -0300

    feat: create product page

--- src/domain/usecases/ProductsUseCases.ts
+++ src/domain/usecases/ProductsUseCases.ts
@@ -4,3 +4,19 @@ import type { Product } from "../repositories/ProductsRepository";
 export const getProducts = (): Promise<Product[]> => {
   return request<Product[]>("GET", "products");
 };
+
+export const updateProduct = (product: Product): Promise<Product> => {
+  return request<Product>("PUT", `products/${product.uid}`, product);
+};
+
+export const createProduct = (
+  product: Omit<Product, "id">
+): Promise<Product> => {
+  return request<Product>("POST", "products", product);
+};
+
+export const deleteProduct = (
+  productId: string
+): Promise<{ success: boolean }> => {
+  return request<{ success: boolean }>("DELETE", `products/${productId}`);
+};
---
+++ src/modules/products/CreateDialog/index.tsx
@@ -0,0 +1,195 @@
+import {
+  type QueryObserverResult,
+  type RefetchOptions,
+} from "@tanstack/react-query";
+import React from "react";
+import { v4 as uuidv4 } from "uuid";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+import type { Product } from "@/domain/repositories/ProductsRepository";
+import { LeafyGreen } from "lucide-react";
+import { useUpdateProductMutation } from "@/hooks/Products/useUpdateProductMutation";
+import { useCreateProductMutation } from "@/hooks/Products/useCreateProductMutation";
+
+interface CreateDialogProps {
+  isOpen: boolean;
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  editableProduct: Product | null | undefined;
+  setEditableProduct: React.Dispatch<React.SetStateAction<Product | null>>;
+  refetchProducts: (
+    options?: RefetchOptions
+  ) => Promise<QueryObserverResult<Product[], Error>>;
+}
+
+export default function CreateDialog({
+  isOpen,
+  setIsOpen,
+  editableProduct,
+  setEditableProduct,
+  refetchProducts,
+}: CreateDialogProps) {
+  const { mutate: updateMutation, isPending: updateIsPending } =
+    useUpdateProductMutation();
+  const { mutate: createMutation, isPending: createIsPending } =
+    useCreateProductMutation();
+
+  console.log({ editableProduct });
+  const handleUpdateProduct = () => {
+    if (editableProduct?.uid) {
+      updateMutation(editableProduct, {
+        onSuccess: () => {
+          refetchProducts();
+          setIsOpen(false);
+          setEditableProduct(null);
+
+          console.log({
+            type: "success",
+            text1: "Produto atualizado com sucesso",
+            position: "bottom",
+          });
+        },
+        onError: () => {
+          console.log({
+            type: "error",
+            text1: "Falha ao atualizar o produto",
+            position: "bottom",
+          });
+        },
+      });
+    } else {
+      createMutation(
+        {
+          uid: uuidv4(),
+          name: editableProduct?.name || "",
+          description: editableProduct?.description || "",
+          averageProductionDays: editableProduct?.averageProductionDays || 0,
+          createdAt: new Date().toISOString(),
+        },
+        {
+          onSuccess: () => {
+            refetchProducts();
+            setIsOpen(false);
+            setEditableProduct(null);
+
+            console.log({
+              type: "success",
+              text1: "Produto criado com sucesso",
+              position: "bottom",
+            });
+          },
+          onError: () => {
+            console.log({
+              type: "error",
+              text1: "Falha ao criar o produto",
+              position: "bottom",
+            });
+          },
+        }
+      );
+    }
+  };
+
+  return (
+    <Dialog
+      open={isOpen}
+      onOpenChange={(open) => {
+        setIsOpen(open);
+        if (!open) setEditableProduct(null);
+      }}
+    >
+      <DialogContent className="min-w-full sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>{editableProduct?.name || ""}</DialogTitle>
+          <DialogDescription className="mt-8 mb-6">
+            <div className="flex flex-col gap-6 w-full">
+              <div className="mx-auto mb-2">
+                <LeafyGreen size={48} />
+              </div>
+              <div className="flex flex-col gap-2 w-full">
+                <Label id="name">Nome do produto</Label>
+                <Input
+                  value={editableProduct?.name || ""}
+                  onChange={(e) =>
+                    setEditableProduct(
+                      (prev) =>
+                        ({
+                          ...prev,
+                          name: e.target.value,
+                        } as Product)
+                    )
+                  }
+                  aria-labelledby="name"
+                />
+              </div>
+              <div className="flex flex-col gap-2 w-full">
+                <Label id="description">Descrição</Label>
+                <Textarea
+                  value={editableProduct?.description || ""}
+                  onChange={(e) =>
+                    setEditableProduct(
+                      (prev) =>
+                        ({
+                          ...prev,
+                          description: e.target.value,
+                        } as Product)
+                    )
+                  }
+                  aria-labelledby="description"
+                />
+              </div>
+              <div className="flex flex-col gap-2 w-full">
+                <Label>Tempo de produção (dias)</Label>
+                <Input
+                  inputMode="numeric"
+                  value={
+                    editableProduct?.averageProductionDays?.toString() || ""
+                  }
+                  onChange={(e) =>
+                    setEditableProduct(
+                      (prev) =>
+                        ({
+                          ...prev,
+                          averageProductionDays: parseInt(e.target.value) || 0,
+                        } as Product)
+                    )
+                  }
+                  aria-labelledby="averageProductionDays"
+                />
+              </div>
+            </div>
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter className="flex flex-row justify-around">
+          <Button
+            size="sm"
+            variant="secondary"
+            onClick={() => {
+              setIsOpen(false);
+              setEditableProduct(null);
+            }}
+          >
+            Fechar
+          </Button>
+
+          <Button
+            size="sm"
+            onClick={handleUpdateProduct}
+            disabled={createIsPending || updateIsPending}
+          >
+            {createIsPending || updateIsPending ? "Carregando..." : "Salvar"}
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
